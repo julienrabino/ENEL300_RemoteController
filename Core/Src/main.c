ee2f9f2a9cf7487bec30580bf2546abd5e5bb24e
julienrabino <julienmescha.rabino@ucalgary.ca>
2026-03-17 08:17:13 -0600

    longer delay for command sending

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -233,7 +233,7 @@ int main(void)
 	        HAL_UART_Transmit(&huart1, &cmd[i], 1, HAL_MAX_DELAY);
 	        HAL_Delay(1); // 1 ms delay
 	    }
-	  HAL_Delay(50);
+	  HAL_Delay(100);
 #endif
 #endif
 
